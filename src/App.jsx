8d7b4ba487c55bf5622027efc7a1e68bcc01eafe
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-12-06 06:39:45 -0500

    Se termino el componente flex-content

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Hero, Sales } from './components';
-import { heroapi, popularsales, topratedsales } from './data/data.js';
+import { Hero, Sales, FlexContent } from './components';
+import { heroapi, popularsales, topratedsales, highlight, sneaker } from './data/data.js';
 
 function App() {
 
@@ -9,7 +9,9 @@ function App() {
       <main className='flex flex-col gap-16 relative'>
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={topratedsales} />
+        <FlexContent endpoint={sneaker}  />
       </main>
     </>
   )
