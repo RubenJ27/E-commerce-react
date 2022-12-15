393419db4fd5b4a10ef39fcf083aa944d2a1a233
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-12-15 08:59:59 -0500

    Correcion de estilos en el nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
           <li className='grid items-center'>
             <button type='button' onClick={onCartToggle} className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
               <ShoppingBagIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
-              <div className={`absolute top-4 right-0 shadow w-4 h-4 text-slate-900 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${navState ? "bg-slate-900 text-slate-100 shadow-slate-900" : "bg-slate-100 text-slate-900 shadow-slate-100"}`}>{totalQTY}</div>
+              <div className={`absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${navState ? 'bg-slate-900 text-slate-100 shadow-slate-900' : 'bg-slate-100 text-slate-900 shadow-slate-100'}`}>{totalQTY}</div>
             </button>
           </li>
         </ul>
