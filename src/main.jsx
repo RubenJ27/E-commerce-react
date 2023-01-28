a700790847886a6831207b22c3ae3874e684443c
Ruben Jaramillo <tiburones.tk@gmail.com>
2023-01-28 12:49:22 -0500

    feat: :bug: Correction the bug of the routes
    Correction the bug of the routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from "react-redux";
-import { Store } from './app';
+import Store from './app/Store';
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
